54ce83df8c9009cef7166d5cb8dc52069b727115
Srikanth Vadlakonda <vsrikanth4780@gmail.com>
2019-03-18 23:17:45 +0530

    Added attributes and getter methods for User

--- crm/src/main/java/com/js/crm/model/User.java
+++ crm/src/main/java/com/js/crm/model/User.java
@@ -1,5 +1,21 @@
 package com.js.crm.model;
 
 public class User {
+	
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
